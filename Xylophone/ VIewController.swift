8b43902b5496232a9b434b4cc4c7d2fb09521954
furixturi <alabebop@gmail.com>
2018-03-04 14:35:24 +0900

    add play audio functionality in notePressed button action handler

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,12 +20,15 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        let soundUrl = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav")
         
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundUrl!)
+        } catch {
+            print(error)
+        }
         
-        
+        audioPlayer.play()
     }
-    
-  
-
 }
 
