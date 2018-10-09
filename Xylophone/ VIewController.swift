df5e4098b6d8c7913dbe4d2b056e11f50de048f3
venomousboxer <y.mohit03031999@gmail.com>
2018-10-09 15:17:37 +0530

    Changes

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController{
     
@@ -15,15 +16,26 @@ class ViewController: UIViewController{
         super.viewDidLoad()
     }
 
-
+    var player: AVAudioPlayer?
+    let url1 = Bundle.main.url(forResource: "note1", withExtension: "wav")!
+    let url2 = Bundle.main.url(forResource: "note2", withExtension: "wav")!
+    let url3 = Bundle.main.url(forResource: "note3", withExtension: "wav")!
+    let url4 = Bundle.main.url(forResource: "note4", withExtension: "wav")!
+    let url5 = Bundle.main.url(forResource: "note5", withExtension: "wav")!
+    let url6 = Bundle.main.url(forResource: "note6", withExtension: "wav")!
+    let url7 = Bundle.main.url(forResource: "note7", withExtension: "wav")!
 
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        let arr = [url1,url2,url3,url4,url5,url6,url7]
+        do{
+            player = try AVAudioPlayer(contentsOf: arr[sender.tag-1])
+            guard let player = player else { return }
+            
+            player.prepareToPlay()
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
-    
-  
-
 }
 
